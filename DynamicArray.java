cb958879b017412d963763a4033fcc346638cbf6
Nurlis <kimbiletovnurlis03@gmail.com>
2023-09-09 20:20:33 +0600

    Added some methods to class DynamicArray

--- DynamicArray.java
+++ DynamicArray.java
@@ -15,6 +15,39 @@ public class DynamicArray<T> implements Iterable<T> {
         arr = (T[]) new Object[capacity];
     }
 
+    public int size() {
+        return len;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public T get(int index) {
+        return arr[index];
+    }
+
+    public void set(int index, T elem) {
+        arr[index] = elem;
+    }
+
+    public void clear() {
+        for (int i = 0; i < len; i++) arr[i] = null;
+        len = 0;
+    }
+
+    public void add(T elem) {
+        // Time to resize!
+        if (len + 1 >= capacity) {
+            if (capacity == 0) capacity = 1;
+            else capacity *= 2; // double the size
+            T[] new_arr = (T[]) new Object[capacity];
+            for (int i = 0; i < len; i++) new_arr[i] = arr[i];
+            arr = new_arr; // arr has extra nulls padded
+        }
+
+        arr[len++] = elem;
+    }
     @Override
     public java.util.Iterator<T> iterator() {
         return new java.util.Iterator<T>() {
