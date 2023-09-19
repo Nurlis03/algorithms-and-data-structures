ae59a9f6b70a05cf452de28bf15982d7ef084031
Nurlis <kimbiletovnurlis03@gmail.com>
2023-09-19 18:46:42 +0600

    Added 10 11 cases

--- DynamicArrayMain.java
+++ DynamicArrayMain.java
@@ -8,18 +8,18 @@ public class DynamicArrayMain {
 
         while (true) {
             System.out.println("\tSelect option:\n" +
-                "\t1. Create Dynamic array.\n" +
+                "\t1. Creating Dynamic array.\n" +
                 "\t2. Size of the Dynamic array.\n" +
-                "\t3. Check if Dynamic array is empty.\n" +
-                "\t4. Get element by index.\n" +
-                "\t5. Set value by index.\n" +
-                "\t6. Clear Dynamic array.\n" +
-                "\t7. Add value.\n" +
-                "\t8. Remove element by index.\n" +
-                "\t9. Remove element by value.\n" +
-                "\t10. Get index of element by value.\n" +
-                "\t11. Check if value contains in Dynamic array.\n" +
-                "\t12. Show Dynamic array.\n" +
+                "\t3. Checking if Dynamic array is empty.\n" +
+                "\t4. Geting element by index.\n" +
+                "\t5. Seting value by index.\n" +
+                "\t6. Clearing Dynamic array.\n" +
+                "\t7. Adding value.\n" +
+                "\t8. Removing element by index.\n" +
+                "\t9. Removing element by value.\n" +
+                "\t10. Geting index of element by value.\n" +
+                "\t11. Checking if value contains in Dynamic array.\n" +
+                "\t12. Showing Dynamic array.\n" +
                 "\t13. Exit.\n\n"
                 );
             System.out.println("Enter operation: ");
@@ -104,6 +104,35 @@ public class DynamicArrayMain {
                         System.out.println("Enter integer value to delete in dynamic array: ");
                         operation = input.nextInt();
                         dynamicArray.remove(operation);
+                    }
+                    else {
+                        System.out.println("Dynamic array not created yet.");
+                    }
+                case 10:
+                    if (dynamicArray != null) {
+                        System.out.println("Enter integer value: ");
+                        operation = input.nextInt();
+                        int indx = dynamicArray.indexOf(operation);
+                        if (indx < 0) {
+                            System.out.println("Value " + operation + "is not exist in dynamic array");
+                        }
+                        else {
+                            System.out.println("The first element found is in the index: " + indx);
+                        }
+                    }
+                    else {
+                        System.out.println("Dynamic array not created yet.");
+                    }
+                case 11:
+                    if (dynamicArray != null) {
+                        System.out.println("Enter integer value: ");
+                        operation = input.nextInt();
+                        if (dynamicArray.contains(operation)) {
+                            System.out.println("Element " + operation + "contains in dynamic array.");
+                        }
+                        else {
+                            System.out.println("Element " + operation + "doesn't contains in dynamic array.");
+                        }
                     }
                     else {
                         System.out.println("Dynamic array not created yet.");
