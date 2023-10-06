9f9f4091d2a3b3a8b12afcd99aecc0789293f709
Nurlis <kimbiletovnurlis03@gmail.com>
2023-10-06 19:54:06 +0600

    added second remove  method

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -173,6 +173,31 @@ public class DoublyLinkedList <T> implements Iterable <T> {
             trav = trav.prev;
         return remove(trav);
     }
+
+    // Remove a particular value in the linked list, O(n)
+    public boolean remove(Object obj) {
+        Node <T> trav = head;
+        
+        // Support searching for null
+        if (obj == null) {
+            for (trav = head; trav != null; trav = trav.next) {
+                if (trav.data == null) {
+                    remove(trav);
+                    return true;
+                }
+            }
+        }
+        // Search for non null object
+        else {
+            for(trav = head; trav != null; trav = trav.next) {
+                if (obj.equals(trav.data)) {
+                    remove(trav);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
     @Override
     public Iterator<T> iterator() {
         // TODO Auto-generated method stub
