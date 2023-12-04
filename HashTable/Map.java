ac5e9e7e129c204aba4d3946816c1d6dfd05000f
Nurlis <kimbiletovnurlis03@gmail.com>
2023-12-04 23:21:38 +0600

    implement class HashNode

---
+++ HashTable/Map.java
@@ -0,0 +1,20 @@
+package HashTable;
+
+// A node of chains
+class HashNode<K, V> {
+    K key;
+    V value;
+    final int hashCode;
+
+    // Reference to next node
+    HashNode<K, V> next;
+
+    public HashNode(K key, V value, int hashCode) {
+        this.key = key;
+        this.value = value;
+        this.hashCode = hashCode;
+    }
+}
+public class Map {
+    
+}
