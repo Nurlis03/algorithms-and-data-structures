9f935cb57d721e842b704e767b3af98a2f7402ef
Nurlis <kimbiletovnurlis03@gmail.com>
2023-12-05 22:52:42 +0600

    Starting to implement Map class

--- HashTable/Map.java
+++ HashTable/Map.java
@@ -1,5 +1,7 @@
 package HashTable;
 
+import java.util.Objects;
+
 // A node of chains
 class HashNode<K, V> {
     K key;
@@ -15,6 +17,40 @@ class HashNode<K, V> {
         this.hashCode = hashCode;
     }
 }
-public class Map {
-    
+
+class Map<K, V> {
+    // bucketArrat is used to store array of chains
+    private ArrayList<HashNode<K, V>> bucketArray;
+
+    // Current capacity of array list
+    private int numBuckets;
+
+    // Current size of array list
+    private int size;
+
+    // Constructor (Initializes capacity, size and empty chains)
+    public Map() {
+        bucketArray = new ArrayList<>();
+        numBuckets = 10;
+        size = 0;
+
+        // Create empty chains
+        for (int i = 0; i < numBuckets; i++) {
+            bucketArray.add(null);
+        }
+    }
+
+    public int size() { return size; }
+    public boolean isEmpty() { return size() == 0; }
+
+    private final int hashCode (K key) {
+        return Objects.hashCode(key);
+    }
+
+    private int getBucketIndex(K key) {
+        int hashCode = hashCode(key);
+        int index = hashCode % numBuckets;
+        index = index < 0 ? index * -1 : index;
+        return index;
+    }
 }
